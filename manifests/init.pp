05137f8e53d52ab6b76872551eaf0567f24e3e7b
Matt Patterson <matt@usrlocal.com>
2019-12-14 23:08:57 -0600

    Small formatting changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,7 @@ class clam( $email = "root", $scan_directory = "/" )
       mode    => 755,
       owner   => "root",
       group   => "root",
-      source => "puppet:///modules/clam/etc/cron.daily/clamscan",
+      source  => "puppet:///modules/clam/etc/cron.daily/clamscan",
       require => Package['clamav'];
   }
 }
